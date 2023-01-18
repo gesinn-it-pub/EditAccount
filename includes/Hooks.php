c1eefb9a8430251f650abf4d3da609e8473cb2a1
gesinn-it-gea <gesinn-it-gea@gesinn.it>
2023-01-18 13:02:02 +0100

    added to Admin Links (fixes #3)

---
+++ includes/Hooks.php
@@ -0,0 +1,18 @@
+<?php
+
+class EditAccountHooks {
+	/**
+	 * Add a link to 'Special:ApprovedPages' to the page
+	 * 'Special:AdminLinks', defined by the Admin Links extension.
+	 */
+	public static function addToAdminLinks( $admin_links_tree ): bool {
+		$general_section = $admin_links_tree->getSection( wfMessage( 'adminlinks_users' )->text() );
+		$extensions_row = $general_section->getRow( 'main' );
+		if ( $extensions_row === null ) {
+			$extensions_row = new ALRow( 'main' );
+			$general_section->addRow( $extensions_row );
+		}
+		$extensions_row->addItem( ALItem::newFromSpecialPage( 'EditAccount' ), 'Userrights' );
+		return true;
+	}
+}
